f2b3c9e833af6444d49b9966196ec2246090b6a0
nirvik <nirvik1993@gmail.com>
2017-09-01 19:26:46 -0400

    cd/exit functionality is completely done

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -9,8 +9,12 @@ char shell[255] = "sbush>";
 char *shell_text = "sbush";
 char *shell_sign = ">";
 char *space = " ";
+int run_status = 1;
 
-char *removeSpaces(char command[]){
+
+char *_removeSpaces(char command[]){
+	// Removes unncessary spaces from the command. 
+	// It basically cleans the command.
 	int j=0;
 	int i=0;
 	int spaces =0;
@@ -35,30 +39,26 @@ char *removeSpaces(char command[]){
 		}
 	}
 	if (j !=0 ) command[j] = '\0';
-	char *formatted_command = command;
+	if(spaces) {
+		command[j-1] = '\0';
+	}
+	//printf("%d difference \n", j);
+	char *formatted_command = (char *)malloc(sizeof(char) * (j));
+	for(int i=0; i<j; i++){
+		formatted_command[i] = command[i];
+		//printf("%s\n", &command[i]);
+	}
+	//char *formatted_command = command;
 	return formatted_command;
 }
 
-char *commandParser(){
-	char ch;
-	int i=0;
-	char command[1024];
-	ch = '\0';
-	while(ch != '\n'){
-		ch = getchar();
-		command[i++] = ch;
-	}
-	command[i-1] = '\0';
-	printf("@commandParser Before removing spaces: %s\n", command);
-	char *cmd = removeSpaces(command);
-	printf("@commandParser After removing spaces: %s\n", cmd);
-	return cmd;
-}
 
-char *_getLastName(char directory[]){
+char *_getBaseName(char directory[]){
+	// This is utility to get the basename of the directory
 	// the directory name could be of these types: /home/people or /home/people/ or "/home/name\'s\\of\ people"
-	// return: people
-	printf("%s is what we get\n", directory);
+
+
+	// printf("%s is what we get\n", directory);
 	int size = strlen(directory);
 	int j=size-1;
 	char name[1024];
@@ -73,17 +73,25 @@ char *_getLastName(char directory[]){
 	for(int i=j+1; i<=last_mark; i++)
 		name[counter++] = directory[i];
 	name[counter] = '\0';
-	char *last_name = name;
-	printf("%s: Last name\n", last_name);
-	return last_name;
+	char *basename;	
+	// if the name contains only '..', then we need to get the basename of the directory
+	if((strlen(name) == 2) && (name[0] == CURRENT) && (name[1] == CURRENT)){
+		size_t size;
+		basename = getcwd(basename, size); 
+		basename = _getBaseName(basename);
+	} else {
+		basename = name;
+	}
+	// printf("%s: Basename\n", basename);
+	return basename;
 }
 
 
 void modifyShellPrompt(char directory[], char *type){
 	char *temp;
-	printf("trying to change shell prompt %s\n", type);
+	// printf("trying to change shell prompt %s\n", type);
 	if(strcmp(type, "cd") == 0){
-		temp = _getLastName(directory);
+		temp = _getBaseName(directory);
 		strcpy(shell, shell_text);
 		strcat(shell, space);
 		strcat(shell, temp);
@@ -92,8 +100,9 @@ void modifyShellPrompt(char directory[], char *type){
 	}
 }
 
-void interpretCommand(char query[]){
-	char command[1024];
+void interpretCommand(char *query){
+	// printf("query is %s\n", query);
+	char command[100];
 	char arguments[1024];
 	int j=0;
 	int i=0;
@@ -103,24 +112,29 @@ void interpretCommand(char query[]){
 	}
 	command[j] = '\0';
 	j++;  // point to the arguments if any
-	printf("command is %s\n", command);	
+
 	while(query[j] != '\0'){
 		arguments[i] = query[j];
 		j++;
 		i++;
 	}
 	arguments[i] = '\0';
-	printf("argument is %s\n", arguments);	
+	// printf("the parsed command is %s\n", command);
+	// printf("the arguments are %s\n", arguments);	
 	if(strcmp(command, "pwd") == 0) {
 		char *buffer = NULL;
 		size_t size;
 		buffer = getcwd(buffer, size);
 		printf("%s %s \n", shell, buffer);
+	} else if(strcmp(command, "exit") == 0) {
+		run_status = 0;
 	}
 	else if(strcmp(command, "cd") == 0){
-		printf("it is a cd command\n");
 		char *directory = arguments;
-		if(directory[0] == REL || directory[0] == CURRENT){
+		// If we find any wildcard entry like '~' or '.' 
+		if(directory[0] == REL /*|| 
+			(directory[0] == CURRENT && directory[1] == '\0') ||
+				(directory[0] == CURRENT && directory[1] != '\0' && directory[1] != CURRENT) */){
 			int j = 0;
 			char temp[255];
 			// copy the path name following the REL(~) sign
@@ -130,36 +144,54 @@ void interpretCommand(char query[]){
 			}
 			temp[j] = '\0';
 			if(directory[0] == REL) strcpy(directory, getenv("HOME"));
+			/*			
 			else {
+				// can ignore this 
 				size_t size;
 				directory = NULL;
 				directory = getcwd(directory, size);
-				printf("IDIOT %s\n", directory);
 			}
+			*/
 			strcat(directory, temp);
-			printf("Translated directory name: %s\n", directory);
+			// printf("Translated directory name: %s\n", directory);
 		}
 		int ret = chdir(directory);
 		if(ret == 0){
-			printf("directory changed \n");
+			// printf("directory changed \n");
 			modifyShellPrompt(directory, "cd");
 		} else {
 			printf("sbush: cd: %s: No such file or directory\n", directory);
+			printf("%s \n", shell);
 		}
 	}
 }
 
+char* commandParser(){
+	char ch;
+	int i=0;
+	char command[1024];
+	ch = '\0';
+	while(ch != '\n'){
+		ch = getchar();
+		command[i] = ch;
+		i++;
+	}
+
+	command[i] = '\0';
+	command[i-1] = '\0';
+	char *cmd = _removeSpaces(command);
+	return cmd;
+}
+
 int main(int argc, char* argv[]) {
-  //char *directory = "/home/nghosh/";
-  //int ret = chdir(directory);
 	/*
-  if(ret == 0){
-    char pwd[1024];
-    if(getcwd(pwd, sizeof(pwd)) != NULL)
       printf("sbush> %s\n", pwd);
-  }
 	*/
-	while(1){
+	char basename[1024];
+	if(getcwd(basename, sizeof(basename)) != NULL){
+		modifyShellPrompt(basename, "cd");
+	}
+	while(run_status){
 		char *command = commandParser();
 		interpretCommand(command);
 	}
