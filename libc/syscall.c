67233a24b9fb7c5f8052a7ddc4e5a6b0e3415892
nihalharish <nihalharish@gmail.com>
2017-09-06 03:07:53 -0400

    tested working malloc

--- libc/syscall.c
+++ libc/syscall.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <sys/syscall.h>
+#include <unistd.h>
+#include <syscall.h>
+#include <sys/mman.h>
 // works
 int syscall_write(int fd, const void *msg , size_t size)
 {
@@ -94,21 +95,36 @@ char* syscall_getcwd ( char *buf , size_t size)
 	);
 }
 
-void* syscall_mmap ( unsigned lond addr, unsigned long len, unsigned long prot, unsigned long flags, unsigned long fd, unsigned long offset){
+void* syscall_mmap ( unsigned long addr, unsigned long len, unsigned long prot, unsigned long flags, unsigned long fd, unsigned long offset){
     
         asm (
         "movl $9, %%eax;"
         "movl %0, %%ebx;"
         "movl %1, %%ecx;"
-        "movl %2, %%edx",
-        "movl %3, %%r10",
-        "movl %4, %%r8",
-        "movl %5, %%r9",
+        "movl %2, %%edx;"
+        "mov %3, %%r10;"
+        "mov %4, %%r8;"
+        "mov %5, %%r9;"
         "syscall;"
         :
         :"m" (addr), "m"(len), "m"(prot), "m"(flags), "m"(fd), "m"(offset)
         :
         );
+}
+
+
+//wrong
+int syscall_munmap( unsigned long addr, size_t len)
+{
+    asm(
+    "movl $11, %%eax;"
+    "movl %0, %%ebx;"
+    "movl %1, %%ecx;"
+    "syscall;"
+    :
+    :"m"(addr), "m"(len)
+    :
+    );
 }
 
 pid_t syscall_fork()
@@ -139,7 +155,7 @@ int syscall_execvpe( const char *filename , char *const argv[], char *const envp
 }
 
 
-//TODO
+//TODO pid_t issue
 pid_t syscall_waitpid(pid_t pid, int *status, int options) {
 	asm(
 		"movl $61,%%eax;"
@@ -155,7 +171,7 @@ pid_t syscall_waitpid(pid_t pid, int *status, int options) {
 }
 
 
-// works
+
 pid_t syscall_getpid()
 {
 	asm ("movl $39, %%eax;"
@@ -203,7 +219,7 @@ int syscall_mkdir(const char *pathname,int mode)
 	);
 }
 
-// works
+
 int syscall_pipe(int *filedes)
 {
 	asm ("movl $22 , %%eax;"
@@ -238,69 +254,30 @@ int syscall_dup2(unsigned int old_fd, unsigned int new_fd){
 	);
 }
 
+void* malloc (size_t size){
+    size_t *mem_pointer;
+    mem_pointer = syscall_mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+
+    *mem_pointer = size;
+    return (void*)(&mem_pointer[1]);
+}
+
+/*
+void free (void* mem_pointer){
+    int *pointer_length = (int*)mem_pointer;
+    size_t size;
+
+    size = *pointer_length;
+    syscall_munmap((void*)pointer_length, size);
+    printf("Custom Free Call\n");
+}
+*/
+
 int main ()
 {
-	// pid works: verified
-	//pid_t pidOne = syscall_getpid();
-	printf("pid: %d\n", pidOne);
-
-	// cwd works: verified
-	/*
-	char *buffer = (char *)malloc(1024);
-	int cwd = syscall_getcwd(buffer, 1024);
-	printf("getcwd: %s %d\n", buffer, cwd);
-	*/
-	// execvpe works: verified
-	/*
-	char *ls[] = {"ls", "-ltr", NULL};
-	char *cmd = "/bin/ls";
-	int ok = syscall_execvpe(cmd, ls, NULL);
-	printf("\n execvpe: %d", ok);
-	*/
-
-	// pipe works: verified
-	/*
-	int fd[2];
-	int p = syscall_pipe(fd);
-	printf("\npipe: %d", p);
-	*/
-
-	// write: working: verfied
-	/* 
-	char *q = "hello world\n";
-	syscall_write(1, q, strlen(q));
-	*/
-
-	// chdir: Works: verified
-	/* 
-	const char *filename = "/home";
-	int s = syscall_chdir(filename);
-	printf("\nchdir: %d", s);
-	*/ 
-	// read: not working 
-	/*
-	char msg[1024] = "can you read me ?";
-	int o = syscall_read(1, msg, strlen(msg));
-	printf("%d \n", o);
-	*/
-
-	pid_t pid = syscall_fork();
-	printf("%d is generated\n", pid);
-	int status;
-	if(pid == 0){
-		char *ls[] = {"ls", "-ltr", NULL};
-		char *cmd = "/bin/ls";
-		int ret = syscall_execvpe(cmd, ls, NULL);
-		syscall_exit(ret);
-	} else if(pid > 0){
-		printf("%d\n", pid);
-		if(syscall_waitpid(pid, &status, 0) > 0){
-			syscall_write(1, "it worked\n", 10);
-		} else {
-			syscall_write(1, "it didnt\n", 10);
-		}
-		//printf("%d\n", syscall_waitpid(pid, &status, 0));
-	}
-    
-return 0;
+    char* test = (char*)malloc(1024*sizeof(char*));
+    char* tester = "Copy This sjnc d noidcjcn ca jnocnvobuabdvubqubvaubvobjbvbjavb bub";
+    strcpy(test, tester);
+    printf("Test: %s\n", test);
+    return 0;
 }
