e9fca3f4fbfa6871c5627e3589284d901d0d0fa0
nihalharish <nihalharish@gmail.com>
2017-09-08 03:34:15 -0400

    new

--- crt/crt1.c
+++ crt/crt1.c
@@ -2,8 +2,16 @@
 
 void _start(void) {
   // call main() and exit() here
-  char *env[] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL, "PATH=/home/nghosh/bin:/home/nghosh/.local/bin:/shared/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games", NULL, NULL, "HOME=/home/nghosh", NULL};
-  main(0, NULL, env);
+  //char *env[] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL, "PATH=/home/nghosh/bin:/home/nghosh/.local/bin:/shared/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games", NULL, NULL, "HOME=/home/nghosh", NULL};
+  int argc = -7;
+  __asm__(
+    "movq 3(%%rsp), %%rdi;"
+    "movq %%rdi, %0;"
+    :"=m"(argc)
+    :
+    :
+  );
+  main(argc, NULL, NULL);
 
   __asm__(
      "movl $60, %%eax;"
