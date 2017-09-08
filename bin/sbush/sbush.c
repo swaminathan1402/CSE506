6fd695339e5ef0a0c6dc2c807d40d142a4821696
swaminathan1402 <swaminathangur.na@stonybrook.edu>
2017-09-08 14:30:13 -0400

    Modified crt1.c

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -3,7 +3,10 @@
 #include <syscall.h>
 
 
-int main(int argc, char* argv[], char* envp[]){
-    syscall_write(1, argv[argc+1], 17);
-    return 0;
+int main(int argc, char* argv[], char* envp[]){  
+  syscall_write(1,"Hello world\n",12);
+ //  for (i=0;i<17;i++)
+  // syscall_write(1, envp[i], 1024)1
+syscall_write(1,argv[0],2);
+return 0;
 }
--- crt/crt1.c
+++ crt/crt1.c
@@ -8,15 +8,16 @@ void _start(void) {
   char* envp[] = {"None"};
 
 __asm__(
-    "addq $0x28, %%rsp;"
-    "popq %%rdi;"
+    "addq $0x28,%%rsp;"
+    "movq (%%rsp),%%rdi;"
     "movq 8(%%rsp), %%rsi;"
+    "movq 16(%%rsp,%%rdi,8 ),%%rdx;"
     "movq %%rdi, %0;"
     "movq %%rsi, %1;"
     "movq %%rdx, %2;"
     :"=m"(argc), "=m"(argv), "=m"(envp)
     :
-    :
+    :"memory"
   );
   main(argc, argv, envp);
 
