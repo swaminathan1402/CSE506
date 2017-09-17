eab5be9b0710d10e7867aab92d1df8ead8072a69
nihalharish <nihalharish@gmail.com>
2017-09-17 17:27:54 -0400

    Mapped Ascii to scancodes

--- sys/keyboard_handler.c
+++ sys/keyboard_handler.c
@@ -3,7 +3,10 @@
 
 int getCharfromCode(unsigned char scan_code1)
 {
-	if(scan_code1 == 0x02){
+        if(scan_code1 == 0x01){
+            return 48;
+        }
+	else if(scan_code1 == 0x02){
 		return 49;
 	}
 	else if(scan_code1 == 0x03){
@@ -13,130 +16,106 @@ int getCharfromCode(unsigned char scan_code1)
 		return 51;
 	}
 	else if(scan_code1 == 0x05){
-		return 51;
+		return 52;
+	}
+	else if(scan_code1 == 0x06){
+		return 53;
+	}
+	else if(scan_code1 == 0x07){
+		return 54;
+	}
+	else if(scan_code1 == 0x08){
+		return 55;
+	}
+	else if(scan_code1 == 0x09){
+		return 56;
+	}
+	else if(scan_code1 == 0x0A){
+		return 57;
+	}
+	else if(scan_code1 == 0x0B){
+		return 58;
+	}
+	else if(scan_code1 == 0x10){
+		return 59;
+	}
+	else if(scan_code1 == 0x1E){
+		return 97;
+	}
+	else if(scan_code1 == 0x30){
+		return 98;
+	}
+	else if(scan_code1 == 0x2E){
+		return 99;
+	}
+	else if(scan_code1 == 0x12){
+		return 100;
+	}
+	else if(scan_code1 == 0x21){
+		return 101;
+	}
+	else if(scan_code1 == 0x22){
+		return 102;
+	}
+	else if(scan_code1 == 0x23){
+		return 103;
+	}
+	else if(scan_code1 == 0x17){
+		return 104;
+	}
+	else if(scan_code1 == 0x24){
+		return 105;
+	}
+	else if(scan_code1 == 0x25){
+		return 106;
+	}
+	else if(scan_code1 == 0x26){
+		return 107;
+	}
+	else if(scan_code1 == 0x32){
+		return 108;
+	}
+	else if(scan_code1 == 0x31){
+		return 109;
+	}
+	else if(scan_code1 == 0x18){
+		return 110;
+	}
+	else if(scan_code1 == 0x19){
+		return 111;
+	}
+	else if(scan_code1 == 0x10){
+		return 112;
+	}
+	else if(scan_code1 == 0x13){
+		return 113;
+	}
+	else if(scan_code1 == 0x1F){
+		return 114;
+	}
+	else if(scan_code1 == 0x14){
+		return 115;
+	}
+	else if(scan_code1 == 0x16){
+		return 116;
+	}
+	else if(scan_code1 == 0x2F){
+		return 117;
+	}
+	else if(scan_code1 == 0x11){
+		return 118;
+	}
+	else if(scan_code1 == 0x2D){
+		return 119;
+	}
+	else if(scan_code1 == 0x15){
+		return 120;
+	}
+	else if(scan_code1 == 0x2C){
+		return 121;
 	}
 	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
-	}
-	else if(scan_code1 == 0x04){
-		return 51;
+		return 122;
 	}
 	return 0;
 }
