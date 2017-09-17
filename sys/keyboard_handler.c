d4faa79a3e4b6a2f6c382325f5912c6b242c3f65
swaminathan1402 <swaminathangur.na@stonybrook.edu>
2017-09-17 16:58:56 -0400

    scan codes

--- sys/keyboard_handler.c
+++ sys/keyboard_handler.c
@@ -1,8 +1,164 @@
 #include <sys/kprintf.h>
 #include <sys/pic.h>
+
+int getCharfromCode(unsigned char scan_code1)
+{
+	if(scan_code1 == 0x02){
+		return 49;
+	}
+	else if(scan_code1 == 0x03){
+		return 50;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x05){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	else if(scan_code1 == 0x04){
+		return 51;
+	}
+	return 0;
+}
+
+
 void keyboard_handler(){
-	//unsigned char scan_code = inb(0x60);
-	inb(0x60);
-	kprintf("hello");
+	unsigned char scan = inb(0x60);
+	//inb(0x60);
+	char map;
+	int code = getCharfromCode(scan);
+	if(code != 0) {
+	  map = (char)code;
+	  kprintf("hello %c", map);
+	}
 	end_of_interrupt(0x0);	
 }
+/*
+int Shiftflag=0;
+int Controlflag=0;
+struct scan_code_entry
+{
+int scan_code;
+char mapping;
+}sct[100]; 
+*/
