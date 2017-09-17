3dffe16a9ccc263757025a51ece3252096fede00
nirvik <nirvik1993@gmail.com>
2017-09-17 17:04:02 -0400

    amazing progress

--- sys/keyboard_handler.c
+++ sys/keyboard_handler.c
@@ -2,7 +2,10 @@
 #include <sys/pic.h>
 void keyboard_handler(){
 	//unsigned char scan_code = inb(0x60);
-	inb(0x60);
-	kprintf("hello");
+ 	unsigned char scan_code = inb(0x60);
+	if(scan_code == 0x04){
+		kprintf("3 is pressed ");
+	}
+	//kprintf("hello %x ",scan_code);
 	end_of_interrupt(0x0);	
 }
