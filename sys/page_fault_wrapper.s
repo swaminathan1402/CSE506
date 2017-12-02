5632a2f554837def6ba547e2e96c92ec7902b383
swaminathan1402 <swaminathangur.na@stonybrook.edu>
2017-12-02 12:20:03 -0500

    Merging

--- sys/page_fault_wrapper.s
+++ sys/page_fault_wrapper.s
@@ -1,7 +1,7 @@
 .global page_fault_wrapper
 .align 8
 page_fault_wrapper:
-cli
+
 push %rax
 push %rbx
 push %rcx
@@ -9,7 +9,7 @@ push %rdx
 push %rbp 
 push %rsi 
 push %rdi 
-cld 
+ 
 call page_fault
 pop %rdi  //Fault code 
 pop %rdi 
